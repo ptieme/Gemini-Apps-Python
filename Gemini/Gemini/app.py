cd042e4efb814b530c5aa9a0b3681efc3c9e9374
ptieme <vaneltieme@gmail.com>
2025-08-25 00:21:11 +0200

    Delete Gemini/Gemini directory
    Unnötige unterordner gelöscht

--- Gemini/Gemini/app.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai 
-
-genai.configure(api_key="AIzaSyCG2c87mxzHBscZCmIPykKRPj4xiavV3XY")
-model = genai.GenerativeModel(model_name="models/gemini-1.5-flash")
-
-
-response = model.generate_content("Liste 5 ingrédients pour faire un bon Ramen.", stream=True)
-for chunk in response:
-    print(chunk.text)
