f60561ed91878f13471274cbaca108bc390771cf
ptieme <vaneltieme@gmail.com>
2025-08-25 00:30:51 +0200

    Update app_image.py

--- Gemini/app_image.py
+++ Gemini/app_image.py
@@ -1,9 +1,9 @@
 import google.generativeai as genai 
 import PIL
 
-genai.configure(api_key="AIzaSyCG2c87mxzHBscZCmIPykKRPj4xiavV3XY")
+genai.configure(api_key="Deine API_Key")
 model = genai.GenerativeModel(model_name="models/gemini-1.5-flash")
 img = PIL.Image.open("images/fantasy.jpeg")
 
-response = model.generate_content(["écris-moi une histoire fantastique pour un enfant d'après l'image", img])
+response = model.generate_content(["Kannst das Bild analysieren?", img])
 print(response.text)
