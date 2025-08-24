92c3bcddc9f18d7f16e4f0d4bc8c5c9057f53a7b
ptieme <vaneltieme@gmail.com>
2025-08-25 00:29:28 +0200

    Update app.py
    API_Key wurde dann entfernt

--- Gemini/app.py
+++ Gemini/app.py
@@ -1,9 +1,9 @@
 import google.generativeai as genai 
 
-genai.configure(api_key="AIzaSyCG2c87mxzHBscZCmIPykKRPj4xiavV3XY")
+genai.configure(api_key="Deine API_Key")
 model = genai.GenerativeModel(model_name="models/gemini-1.5-flash")
 
 
-response = model.generate_content("Liste 5 ingrédients pour faire un bon Ramen.", stream=True)
+response = model.generate_content("Listet mal 5 Zutaten mit dem man ein Pizza machen kann.", stream=True)
 for chunk in response:
     print(chunk.text)
